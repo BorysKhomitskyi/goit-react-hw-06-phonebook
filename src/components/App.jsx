e117317abf9ec45d6afc11d0a1d59572b6ad0103
BorysKhomitskyi <borys2130@gmail.com>
2023-04-27 23:56:54 +0300

    hw6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,69 +1,23 @@
-import { nanoid } from 'nanoid';
-import { useState, useEffect } from 'react';
-import ContactForm from './ContactForm';
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/contacts-slice';
+import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
 import Message from './Message';
 import css from './App.module.css';
 
 function App() {
-  const basicContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-
-  const [filter, setFilter] = useState('');
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(window.localStorage.getItem('contacts')) ?? basicContacts;
-  });
-
-  useEffect(() => {
-    const json = JSON.stringify(contacts);
-    localStorage.setItem('contacts', json);
-  }, [contacts]);
-
-  const addContact = ({ name, number }) => {
-    const newContact = { id: nanoid(), name, number };
-
-    contacts.some(contact => contact.name === name)
-      ? alert(`${name} is already in contacts!`)
-      : setContacts(prevContacts => [newContact, ...prevContacts]);
-  };
-
-  const deleteContact = contactId => {
-    setContacts(prevContacts =>
-      prevContacts.filter(contact => contact.id !== contactId)
-    );
-  };
-
-  const changeFilter = e => setFilter(e.currentTarget.value);
-
-  const filtredContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
+  const contacts = useSelector(getContacts);
 
   return (
     <div className={css.container}>
       <h1 className={css.title}>
         Phone<span className={css.title__color}>book</span>
       </h1>
-      <ContactForm onSubmit={addContact} />
-
+      <ContactForm />
       <h2 className={css.subtitle}>Contacts</h2>
-      <Filter filter={filter} changeFilter={changeFilter} />
-      {contacts.length > 0 ? (
-        <ContactList
-          contacts={filtredContacts()}
-          onDeleteContact={deleteContact}
-        />
-      ) : (
-        <Message text="Contact list is empty." />
-      )}
+      <Filter />
+      {contacts.length > 0 ? <ContactList /> : <Message />}
     </div>
   );
 }
